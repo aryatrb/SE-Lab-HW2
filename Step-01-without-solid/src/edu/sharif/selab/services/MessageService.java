a4d522d98086a9b147ec4ced4efd206ad8d0e7eb
kiana-msz <kianamousazadeh2018@gmail.com>
2024-10-30 21:01:51 +0330

    Debug Part 1

--- Step-01-without-solid/src/edu/sharif/selab/services/MessageService.java
+++ Step-01-without-solid/src/edu/sharif/selab/services/MessageService.java
@@ -7,5 +7,5 @@ import edu.sharif.selab.models.TelegramMessage;
 public interface MessageService {
     public void sendSmsMessage(SmsMessage smsMessage);
     public void sendEmailMessage(EmailMessage emailMessage);
-    void sendTelegramMessage(TelegramMessage telegramMessage);
+    public void sendTelegramMessage(TelegramMessage telegramMessage);
 }
